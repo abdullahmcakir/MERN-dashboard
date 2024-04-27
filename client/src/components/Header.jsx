cbbe2b0ff74e939f062301d8ed4dc889fd310eff
Abdullah <abdullah_mcakir@outlook.com>
2024-04-27 21:29:35 +0300

    front-end installation-react setup

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <header id='header' className='header fixed-top d-flex align-items-center'>
+       
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Logo.tsx"
+
+function Logo() {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
