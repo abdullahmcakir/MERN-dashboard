c24fcbdaef6f6b5c96feff1cc54e923152cb235d
Abdullah <abdullah_mcakir@outlook.com>
2024-05-09 20:43:24 +0300

    cards finished

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
 import './dashboard.css';
+import Reports from './Reports';
 
 function Dashboard() {
+  const [cards, setCards] = useState([])
+
+  const fetchData = () => {
+    fetch('http://localhost:4000/cards')
+      .then(res => res.json())
+      .then(data =>{
+        setCards(data);
+      })
+      .catch(e => console.log(e.message));
+  };
+  
+    useEffect(() => {
+      fetchData();
+    },  []);
+
   return (
     <section className='dashboard section'>
         <div className="row">
             <div className="col-lg-8">
-                <div className="row"></div>
+                <div className="row">
+                  {
+                    cards && cards.length>0 &&
+                    cards.map(card=><Card key={card._id} card={card} />)
+                  }
+                  <div className="col-12">
+                    <Reports />
+                  </div>
+                </div>
             </div>
             <div className="col-lg-4"></div>
         </div>
